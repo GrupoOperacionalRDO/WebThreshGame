0a72f96defcaff6321f0562eb11b0cde2f7e428b
Lorek <lorekqt@live.com>
2015-03-20 01:32:06 -0300

    Script da movimentação e spawn aleatório a cada 1.5s

---
+++ Assets/Scenes/Resources/Scripts/SoulBehavior.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoulBehavior : MonoBehaviour {
+
+	public float speed;
+
+	void Start () {
+		gameObject.GetComponent<Rigidbody> ().velocity = transform.right * speed;
+	}
+	
+
+	void Update () {
+
+			
+	}
+		
+	protected void OnTriggerEnter(Collider collider)
+		{
+		if (collider.gameObject.tag!="Soul")
+			{
+				DestroyObject(this.gameObject);
+			}
+	}
+	}
---
+++ Assets/Scenes/Resources/Scripts/SoulSpawnController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoulSpawnController : MonoBehaviour {
+
+	public GameObject soul;
+	private int count;
+	public float respawntime;
+	void Start () 
+	{
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	if (++count >= respawntime * 60) {
+			count=0;
+			float z = Random.Range (-5.0F, 4.5F);
+			float x = Random.Range (-4.0F, 1.0F);
+			Instantiate(soul, new Vector3(x, 1, z), Quaternion.identity);
+			
+		}
+	}
+}
