25348258ea343d4bc94abdc6f328ae70a7bc787e
arturnista <arturnista@gmail.com>
2015-04-06 00:12:08 -0300

    Necessidade dos Souls e Adicionado texto do mesmo

--- Assets/Scenes/Resources/Scripts/UpgradeController.cs
+++ Assets/Scenes/Resources/Scripts/UpgradeController.cs
@@ -3,13 +3,14 @@ using System.Collections;
 
 public class UpgradeController : MonoBehaviour {
 
-	public float speed;
-	public float range;
-	public int inbound;
+	public float speed, speedLevel;
+	public float range, rangeLevel;
+	public int inbound, inboundLevel;
 	void Start () {
 		speed = 100F;
 		range = 100F;
 		inbound = 1;
+		speedLevel = rangeLevel = inboundLevel = 1;
 	}
 	//Get's
 	public float getSpeed()
@@ -27,15 +28,40 @@ public class UpgradeController : MonoBehaviour {
 	//Increase's
 	void increaseSpeed()
 	{
-		speed+= 5;	
+		if (GameObject.FindGameObjectWithTag ("HUD").GetComponent<HUDController> ().getScore () >= getValorSpeed ()) {
+			GameObject.FindGameObjectWithTag ("HUD").GetComponent<HUDController> ().removeScore (getValorSpeed ());
+			speed += 5;	
+			speedLevel++;
+		}
 	}
 	void increaseRange()
 	{
-		range+= 5;	
+		if (GameObject.FindGameObjectWithTag ("HUD").GetComponent<HUDController> ().getScore () >= getValorRange ()) {
+			GameObject.FindGameObjectWithTag ("HUD").GetComponent<HUDController> ().removeScore (getValorRange ());
+			range += 5;	
+			rangeLevel++;
+		}
 	}
 	void increaseInbound()
 	{
-		inbound++;	
+		if (GameObject.FindGameObjectWithTag ("HUD").GetComponent<HUDController> ().getScore () >= getValorInbound ()) {
+			GameObject.FindGameObjectWithTag ("HUD").GetComponent<HUDController> ().removeScore (getValorInbound ());
+			inbound++;	
+			inboundLevel++;
+		}
+	}
+	//Calculo dos valores
+	public float getValorSpeed()
+	{
+		return Mathf.Pow (speedLevel,2);
+	}
+	public float getValorRange()
+	{
+		return Mathf.Pow (rangeLevel,2);
+	}
+	public float getValorInbound()
+	{
+		return Mathf.Pow (inboundLevel,2);
 	}
 
 
--- Assets/Scenes/Resources/Scripts/UpgradePanelController.cs
+++ Assets/Scenes/Resources/Scripts/UpgradePanelController.cs
@@ -4,12 +4,20 @@ using System.Collections;
 
 public class UpgradePanelController : MonoBehaviour {
 
+
+	public Text rangeValueUpgrade;
+	public Text speedValueUpgrade;
+	public Text inboundValueUpgrade;
 	// Update is called once per frame
 	void Update(){
 
 		Text scoretext = GameObject.Find ("Score").GetComponent<Text> ();
 		float scorevalue = GameObject.FindGameObjectWithTag ("HUD").GetComponent<HUDController> ().getScore();
 		scoretext.text = scorevalue.ToString() + " souls.";
+		//Valores
+		rangeValueUpgrade.text = GameObject.FindGameObjectWithTag ("UpdateController").GetComponent<UpgradeController> ().getValorRange().ToString();
+		speedValueUpgrade.text = GameObject.FindGameObjectWithTag ("UpdateController").GetComponent<UpgradeController> ().getValorSpeed().ToString();
+		inboundValueUpgrade.text = GameObject.FindGameObjectWithTag ("UpdateController").GetComponent<UpgradeController> ().getValorInbound().ToString();
 
 	}
 }
